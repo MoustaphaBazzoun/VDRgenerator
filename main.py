b383e969e1fecafb67c4a3e7a1c17dde9d242d48
Mousta <mousta_bazzoun@outlook.com>
2024-03-29 13:56:05 -0300

    Implemented importing dataroom structure from excel, project is Working now. But need some adjustments and error handling.

--- main.py
+++ main.py
@@ -1,10 +1,38 @@
 import pandas as pd
 import os
 
-#df = pd.read_excel("potato.xlsx")
+df = pd.read_excel("potato.xlsx", header=0, converters={"Number":str, "File":str})
 
-#print(df.head())
+dfToDict = {}
 
+for index, row in df.iterrows():
+    dfToDict[row['Number']] = row['File']
+
+
+#Basic Logic
+"""
+Notes:
+- Maybe temp and result can be improved
+- Split some logic into small functions to make it easier to read
+"""
+
+for key in dfToDict:
+    splittedA = key.split(sep=".")
+    result = "./DataRoom"
+    temp = ""
+    for n in splittedA:
+        if temp == "":
+            temp = str(n)
+        else:
+            temp = temp + "." + str(n)
+        result = result + "/" + str(temp) + "." + str(dfToDict[temp])
+    print(result)
+    os.mkdir(result)
+
+
+#----------------------------
+#Dummy Data
+"""
 testDict = {
     '1':'Financials',
     '1.1':'Income Statement',
@@ -16,23 +44,4 @@ testDict = {
     '2.3':'Payment Roll',
     '3':'Legal',
 }
-
-#Basic Logic
 """
-Notes:
-- Maybe temp and result can be improved
-- Split some logic into small functions to make it easier to read
-"""
-for key in testDict:
-    splittedA = key.split(sep=".")
-    result = "./DataRoom"
-    temp = ""
-    for n in splittedA:
-        if temp == "":
-            temp = str(n)
-        else:
-            temp = temp + "." + str(n)
-        result = result + "/" + str(temp) + "." + str(testDict[temp])
-    print(result)
-    os.mkdir(result)
-
